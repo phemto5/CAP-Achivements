71ae899b9a946f9d9d676d04d34662eb23b823ba
Nicholas <nicholasaa@gmail.com>
2023-08-14 20:28:00 -0700

    installed pandas and loaded some data

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello");
+# print("hello");
+import pandas as pd
+# import csv
+
+filePath = 'Spokane_Squadron_promotion_report_8_9_2023.csv'
+
+with open(filePath) as file:
+    # csvFile = csv.reader(file)
+    # for lines in csvFile:
+    #     print(lines)
+    dataFrame = pd.read_csv(filePath, sep=",")
+
