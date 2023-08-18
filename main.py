cc64c45c928e12ef9472821a85233c0191747135
Nicholas <nicholasaa@gmail.com>
2023-08-17 22:00:43 -0700

    updated to use pandas and not math/ finds correct rows to print

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # print("hello");
 import pandas as pd
+# import numpy
+import math
 # import csv
 
 filePath = 'Spokane_Squadron_promotion_report_8_9_2023.csv'
@@ -10,3 +12,15 @@ with open(filePath) as file:
     #     print(lines)
     dataFrame = pd.read_csv(filePath, sep=",")
 
+    for index, row in dataFrame.iterrows():
+        print("|------------------------------------------------")
+        print("|"+row["NameLast"] + ", " + row["NameFirst"])
+        print("|-"+"Working on Acivement:" + row["AchvName"])
+        for  kname in dataFrame.columns:
+            if str(row[kname]).isdigit():
+                    if float(row[kname]) == 0:
+                        print(f"|-- {kname}")
+            else:
+                if pd.isna(row[kname]):
+                    print(f"|-- {kname}")
+        print("|------------------------------------------------\n")
